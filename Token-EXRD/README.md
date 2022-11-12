8d72bc91b9a8f0a0cc57da4a2d607fe87ef83a6f
Alex <alessandro.piccione.75@gmail.com>
2022-11-12 16:08:38 +0000

    cleanup

---
+++ Token-EXRD/README.md
@@ -0,0 +1,13 @@
+# Token eXRD
+
+Create an ETH Token on the Goerli test network.
+
+Goerli explorer: https://goerli.etherscan.io
+
+## Solidity compiler version
+
+To use a specific compiler version: right click in the code and select "Solidity: Change workspace compiler version (Remote)"
+
+## Build
+
+`yarn truffle:token:goerli`
--- Token-EXRD/contracts/eXRD.sol
+++ Token-EXRD/contracts/eXRD.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.8;
 
-//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-//import "@openzeppelin/contracts/tok"
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 //import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 //import "../contract-dependencies/ERC20Mintable.sol";
 
 /*is ERC20Burnable, ERC20Mintable*/
 contract eXRD is ERC20 {
-  // amount: initiaSupply?
+  // amount: initial supply?
   constructor(address depositAddress, uint256 amount) public ERC20("Test Three", "tTHR") {
     // address(0) is the same as "0x0", an uninitialized address. Used to burn token.
     require(depositAddress != address(0));
